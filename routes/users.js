51e1c6e16063a36b3201594f2e9531cefaa0407c
Macbook Pro <macbookpro@Tawlur-MBP.hsd1.co.comcast.net>
2020-06-01 12:47:45 -0600

    Setup OATH

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
-var express = require('express');
-var router = express.Router();
+const router = require('express').Router();
+const passport = require('passport');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// The root route renders our only view
+router.get('/', function(req, res) {
+  res.redirect('/');
+});
+
+router.get('/auth/google', passport.authenticate(
+  'google',
+  { scope: ['profile', 'email'] }
+));
+
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/',
+    failureRedirect: '/'
+  }
+));
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
 });
 
 module.exports = router;
